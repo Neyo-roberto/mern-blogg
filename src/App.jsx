277f7b5c70463b286a7a28aaa14d742093cd6caa
niyonkuru roberto <niyonkuroberto@gmail.com>
2024-02-09 17:09:38 +0200

    install reactjs and tailwind css and create the first template

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { MyApp } from './myApp'
+
+export default function App() {
+  return (
+    <MyApp />
+  )
+}
---
+++ src/myApp.jsx
@@ -0,0 +1,6 @@
+export const MyApp = () => {
+  return <div>
+    <h1>roberto niyonkuru</h1>
+  </div>
+};
+
